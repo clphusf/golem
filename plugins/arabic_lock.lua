633004416c3f177e4c7bbb9b4ecaaf9e41af4712
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-06 16:14:20 +0430

    Update arabic_lock.lua

--- plugins/arabic_lock.lua
+++ plugins/arabic_lock.lua
@@ -19,9 +19,9 @@ local function run(msg, matches)
 		local username = msg.from.username
 		local name = msg.from.first_name
 		if username and is_super_group(msg) then
-			send_large_msg(receiver , "Arabic/Persian is not allowed here\n@"..username.."["..msg.from.id.."]\nStatus: User kicked/msg deleted")
+			send_large_msg(receiver , "یوزر @"..username.."["..msg.from.id.."] به دلیل استفاده از عربی یا فارسی از گروه اخراج و پیام هایش پاک شد")
 		else
-			send_large_msg(receiver , "Arabic/Persian is not allowed here\nName: "..name.."["..msg.from.id.."]\nStatus: User kicked/msg deleted")
+			send_large_msg(receiver , "یوزر "..name.."["..msg.from.id.."] به دلیل استفاده از عربی یا فارسی از گروه اخراج و پیام هایش پاک شد")
 		end
 		local name = user_print_name(msg.from)
 		savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (arabic was locked) ")
