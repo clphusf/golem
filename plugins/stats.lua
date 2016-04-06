43b7e4dc0ac13d1d371c27b3d749c921b60b6add
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-06 16:26:55 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -28,7 +28,7 @@ local function chat_stats(receiver, chat_id)
         return a.msgs > b.msgs
     end
   end)
-  local text = 'Users in this chat \n'
+  local text = 'Users in this chat : \n'
   for k,user in pairs(users_info) do
     text = text..user.name..' = '..user.msgs..'\n'
   end
@@ -60,7 +60,7 @@ local function chat_stats2(chat_id)
       end
     end)
 
-  local text = 'Users in this chat \n'
+  local text = 'Users in this chat : \n'
   for k,user in pairs(users_info) do
     text = text..user.name..' = '..user.msgs..'\n'
   end
@@ -87,14 +87,14 @@ local function bot_stats()
 
   hash = 'chat:*:users'
   r = redis:eval(redis_scan, 1, hash)
-  text = text..'\nGroups: '..r
+  text = text..'\nGroups : '..r
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'avira' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /avira ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "avira" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (teleseed)",
-	"^[#!/]([Tt]eleseed)"
+    "^[#!/]([Ss]tats) (avira)",
+    "^[#!/]([Aa]vira)"
     }, 
   run = run
 }
