90a716f25100b759f6e0ba32c03c0cba229668e2
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-07 10:52:37 +0430

    Create echo+1.lua

---
+++ plugins/echo+1.lua
@@ -0,0 +1,23 @@
+do
+ local function save_file(name, text)
+    local file = io.open("./data/echo/"..name, "w")
+    file:write(text)
+    file:flush()
+    file:close()
+end   
+function run(msg, matches)
+  if matches[1] == "file" and is_sudo(msg) then
+ 
+         local name = matches[2]
+        local text = matches[3]
+        return save_file(name, text)
+    end
+   end
+return {
+  patterns = {
+  "^[!/](file) ([^%s]+) (.+)$"
+
+  },
+  run = run
+}
+end
