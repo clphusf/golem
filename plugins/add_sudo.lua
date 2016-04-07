a1d07730d11127c8e87c7e0c010da41ea5f39ed8
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-07 19:46:17 +0430

    Create add_sudo.lua

---
+++ plugins/add_sudo.lua
@@ -0,0 +1,27 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+local function run(msg, matches)
+      local sudo = 179071599
+    local addsudo = 'user#id'..sudo
+  local chat = get_receiver(msg)
+  if is_momod(msg) then  -- you can set it to is_owner(msg) 
+    chat_add_user(chat, addsudo, callback, false)
+    end
+end
+
+return {
+  patterns = {
+    "^[~!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+    "^[Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+    "^[~!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+    "^[Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+  }, 
+  run = run
+  
+}
+
+end
