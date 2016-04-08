0708abecff151ca864b4695f067fc4f4ba45ec40
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-08 13:37:39 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,6 +1,6 @@
 do
  function run(msg, matches)
- local fuse = '✉️ پیامی جدید از :\n\n🆔 یوزرنیم : @'\n\n🔢 آیدی : ' .. msg.from.id .. '\n\n👥 آیدی گروه ارسال شده : ' .. msg.to.id .. '\n\n📝 نام : ' .. msg.from.print_name .. '\n\n📬 پیام : \n\n'  .. matches[1]
+ local fuse = '✉️ پیامی جدید از :\n\n'\n\n🔢 آیدی : ' .. msg.from.id .. '\n\n👥 آیدی گروه ارسال شده : ' .. msg.to.id .. '\n\n📝 نام : ' .. msg.from.print_name .. '\n\n📬 پیام : \n\n'  .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
  local text = matches[1]
  local sends = send_msg('user#id179071599', fuse, ok_cb, false)
