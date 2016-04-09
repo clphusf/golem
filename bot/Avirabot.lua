42004dc4af251b7883b2b7ea886894d2284361c2
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-09 17:35:04 +0430

    Update Avirabot.lua

--- bot/Avirabot.lua
+++ bot/Avirabot.lua
@@ -259,7 +259,7 @@ An advanced administration bot based on TG-CLI written in Lua
     help_text_realm = [[ ]],
     help_text = [[ ]],
 	help_text_super =[[
-🚀 Avira V5 SuperGroup Commands:
+🚀 Avira SuperGroup Commands:
 
 !owner
 دریافت آیدی مدیر اصلی گروه
