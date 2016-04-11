6b095bbf0b9df35c72a2f1f363b5516eaf4afd72
dram1710 <mohammad400ebrahimi@yahoo.com>
2016-04-11 19:41:55 +0430

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,27 @@
+do
+ function run(msg, matches)
+ 
+ local fuse = '✉️ پیامی جدید : \n\n🆔 آیدی : ' .. msg.from.id .. '\n\n👤 نام : ' .. msg.from.print_name ..'\n\n🔢 یوزرنیم : @' .. msg.from.username .. '\n\n📬 پیام :\n' .. matches[1] 
+ local fuses = '!printf user#id' .. msg.from.id
+ 
+ 
+   local text = matches[1]
+   local chat = "channel#id"..1027493813
+   --like : local chat = "channel#id"..12345678
+   
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return 'پیام شما ارسال شد با تشکر'
+ 
+ end
+ end
+ return {
+  
+  description = "Feedback",
+ 
+  usage = "!feedback message",
+  patterns = {
+  "^[!#/][Ff]eedback (.*)$"
+ 
+  },
+  run = run
+ }
