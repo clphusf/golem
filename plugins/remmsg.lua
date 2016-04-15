d6d8b830f5f831ea0f9a2242f7f0cdee62983bd4
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-15 14:57:57 +0430

    Create remmsg.lua

---
+++ plugins/remmsg.lua
@@ -0,0 +1,32 @@
+local function history(extra, suc, result)
+for i=1, #result do
+delete_msg(result[i].id, ok_cb, false)
+end
+if tonumber(extra.con) == #result then
+send_msg(extra.chatid, 'ℹ "'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+else
+send_msg(extra.chatid, 'ℹ️ تمام پیام های سوپر گروه حذف شد', ok_cb, false)
+end
+end
+local function run(msg, matches)
+if matches[1] == 'remmsg' then
+    if permissions(msg.from.id, msg.to.id, "settings") then
+        if msg.to.type == 'channel' then
+            if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
+            return "خطا عدد انتخاب شده باید زیر 100 باشد"
+            end
+            get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        else
+                         return "فقط مخصوص سوپرگروه می باشد"
+        end
+else
+return "فقط مخصوص مدیران گروه می باشد"
+end
+end
+end
+return {
+    patterns = {
+        '^[!/#](remmsg) (%d*)$'
+    },
+    run = run
+}
