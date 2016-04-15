cbd6d2bd7ce9fc427d7b2541fbcdf5513040a610
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-15 13:54:48 +0430

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,9 +1,7 @@
-
-do
-
-function run(msg, matches)
-  return [[
-  👥 نرخ گروه های آنتی اسپم :
+local function run(msg)
+if msg.text == "nerkh" then
+	return [[
+	👥 نرخ گروه های آنتی اسپم :
   
   💴 سوپرگروه یک ماهه 5000 تومان
   💷 سوپرگروه دو ماهه 9000 تومان
@@ -14,12 +12,17 @@ function run(msg, matches)
   @mohammadarak
   ]]
 end
-  patterns = {
-    "^نرخ$"
-    "^[!/#]([Nn]erkh)$"
-    "^[Nn]erkh$"
-  }, 
-  run = run 
-}
-
 end
+ return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+	"^[!/#]([Nn]erkh)",
+		}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
+-- مدیر : @mohammadarak
+-- ربات : @avirabot
+-- هر گونه کپی برداری بدون ذکر منبع حرام است 
