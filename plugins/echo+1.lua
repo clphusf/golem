64149514da188857b42b46c6c8b80ecdd18be4d3
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-15 14:42:59 +0430

    Update echo+1.lua

--- plugins/echo+1.lua
+++ plugins/echo+1.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   if matches[1] == "file" then
     local file = matches[2]
-    if is_sudo(msg) then
+    if is_mmod2(msg) then
       local receiver = get_receiver(msg)
       send_document(receiver, "./data/file/"..file, ok_cb, false)
     end
