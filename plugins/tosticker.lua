a9709c426b4b05c694fdee5043f3d99de2d82b16
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-15 11:38:42 +0430

    Update tosticker.lua

--- plugins/tosticker.lua
+++ plugins/tosticker.lua
@@ -1,7 +1,7 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'sticker/'..msg.from.id..'.webp'
+    local file = './data/sticker/'..msg.from.id..'.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
