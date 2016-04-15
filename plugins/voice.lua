f288b30c5b95a0cc836767fdd687dbbee2612acd
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-15 15:45:59 +0430

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -12,7 +12,7 @@ local text = matches[1]
     text,b = text:gsub(' ','.')
     
     
-  if msg.to.type == 'mmod' then 
+  if msg.to.type == 'momod' then 
       return nil
       else
   local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
