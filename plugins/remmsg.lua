5e29e0abd25f3d156a816c290be414b112f59936
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-16 15:17:57 +0430

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -5,11 +5,11 @@ end
 if tonumber(extra.con) == #result then
 send_msg(extra.chatid, ''..#result..' پیام اخیر سوپر گروه حذف شد', ok_cb, false)
 else
-send_msg(extra.chatid, 'ℹ️ تمام پیام های سوپر گروه حذف شد', ok_cb, false)
+send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد', ok_cb, false)
 end
 end
 local function run(msg, matches)
-if matches[1] == 'msgrem' then
+if is_momod(msg) and if matches[1] == 'msgrem' then
         if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
             return "خطا عدد انتخاب شده باید زیر 100 باشد"
