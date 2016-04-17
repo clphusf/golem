3c227e1911ed5568ad40ca07baa9f7a47dcce5dc
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-17 19:41:32 +0430

    Update azan.lua

--- plugins/azan.lua
+++ plugins/azan.lua
@@ -63,13 +63,14 @@ function run(msg, matches)
 	local code = http.request('http://api.aladhan.com/timings/'..dumptime..'?latitude='..lat..'&longitude='..lng..'&timezonestring=Asia/Tehran&method=7')
 	local jdat = json:decode(code)
 	local data = jdat.data.timings
-	local text = '⛪️شهر: '..city
-	  text = text..'\n🕌اذان صبح: '..data.Fajr
-	  text = text..'\n🕌طلوع آفتاب: '..data.Sunrise
-	  text = text..'\n🕌اذان ظهر: '..data.Dhuhr
-	  text = text..'\n🕌غروب آفتاب: '..data.Sunset
-	  text = text..'\n🕌اذان مغرب: '..data.Maghrib
-	  text = text..'\n🕌عشاء : '..data.Isha
+	local text = 'شهر: '..city
+	  text = text..'\nاذان صبح: '..data.Fajr
+	  text = text..'\nطلوع آفتاب: '..data.Sunrise
+	  text = text..'\nاذان ظهر: '..data.Dhuhr
+	  text = text..'\nغروب آفتاب: '..data.Sunset
+	  text = text..'\nاذان مغرب: '..data.Maghrib
+	  text = text..'\nعشاء : '..data.Isha
+	  text = text..'\n\n@avirabot'
 	if string.match(text, '0') then text = string.gsub(text, '0', '۰') end
 	if string.match(text, '1') then text = string.gsub(text, '1', '۱') end
 	if string.match(text, '2') then text = string.gsub(text, '2', '۲') end
@@ -84,7 +85,7 @@ function run(msg, matches)
 end
 
 return {
-  patterns = {"^[#/!][Aa]zan (.*)$","^[#/!](azan)$"}, 
+  patterns = {"^[/#!][Aa]zan (.*)$","^[/#!](azan)$"}, 
   run = run 
 }
 
