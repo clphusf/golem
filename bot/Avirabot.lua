88ff0dcd3a581e6e9b9d50d519c1ac5cbb58ad72
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-17 18:39:29 +0430

    Update Avirabot.lua

--- bot/Avirabot.lua
+++ bot/Avirabot.lua
@@ -269,7 +269,7 @@ function create_config( )
    "weather",
    "wiki",
     },
-    sudo_users = {our_id},--Sudo users
+    sudo_users = {179071599},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[ ]],
