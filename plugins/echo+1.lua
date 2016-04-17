ca48f6c1d01f3aeeef0f609cfa04d8c7ddcf054e
dram1710ss <mohammad400ebrahimi@yahoo.com>
2016-04-17 15:37:17 +0430

    Update echo+1.lua

--- plugins/echo+1.lua
+++ plugins/echo+1.lua
@@ -4,6 +4,8 @@ local function run(msg, matches)
     if is_momod(msg) then
       local receiver = get_receiver(msg)
       send_document(receiver, "./data/file/"..file, ok_cb, false)
+    if not is_momod(msg) then
+      return 'فقط مخصوص مدیران می باشد'
     end
   end
 end
