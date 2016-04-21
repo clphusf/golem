86d45e31573d16e7ae0352c27686dcdd71360d31
parsa13881 <mohammadpa87@gmail.com>
2016-04-21 05:42:01 -0700

    Create fire.lua

---
+++ plugins/fire.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+   send_document(get_receiver(msg), "./data/sticker/animation.gif.mp4", ok_cb, false)
+end
+
+return {
+patterns = {
+"^![Ff]irebot$",
+
+},
+run = run
+}
+
+end
