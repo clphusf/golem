b4d6b02bb83c61458cace7f27cd51cb10716c600
parsa13881 <mohammadpa87@gmail.com>
2016-04-24 20:34:32 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -2,7 +2,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 local text = 'info'
-local text1 ="🔱group ID : "..msg.from.id.."\n🔱group name : "..msg.to.title.."\n🔱your name : "..(msg.from.first_name or '').."\n🔱first : "..(msg.from.first_name or '').."\n🔱last : "..(msg.from.last_name or '').."\n🔱user ID : "..msg.from.id.."\n🔱username : @"..(msg.from.username or '').."\n🔱phone number : +"..(msg.from.phone or '')
+local text1 ="🔱group ID : "..msg.to.id.."\n🔱group name : "..msg.to.title.."\n🔱your name : "..(msg.from.first_name or '').."\n🔱first : "..(msg.from.first_name or '').."\n🔱last : "..(msg.from.last_name or '').."\n🔱user ID : "..msg.from.id.."\n🔱username : @"..(msg.from.username or '').."\n🔱phone number : +"..(msg.from.phone or '')
   reply_msg(reply_id, text1, ok_cb, false)
 end
 return {
