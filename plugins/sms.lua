f08fbe2232aed5ba8f1df69e3475ec014b060656
parsa13881 <mohammadpa87@gmail.com>
2016-04-24 19:23:00 +0430

    Create sms.lua

---
+++ plugins/sms.lua
@@ -0,0 +1,33 @@
+local function run(msg, matches)
+
+  if not is_sudo(msg) then — For Sudoers only !
+
+    return 'فقط برای سودو ها'
+
+  end
+
+  if matches[1]:lower() == 'sms' and is_sudo(msg) then
+
+    local response = matches[3]
+
+    send_large_msg("user#id"..matches[2], response)
+
+            local receiver = 'user#id'..user_id
+
+        send_large_msg(receiver, response)
+
+  end
+
+  end
+
+return {
+
+  patterns = {
+
+    "^[!/](sms) (%d+) (.*)$"
+
+  },
+
+  run = run
+
+}
