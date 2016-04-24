8d214363f24242077f4cd4a72820a3a9b253c1b3
parsa13881 <mohammadpa87@gmail.com>
2016-04-24 19:48:33 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,32 +1,31 @@
 do
 
 local function run(msg, matches)
-  if matches[1]:lower() == 'me' or "wai" then
+  if matches[1] == 'me' then
     if is_sudo(msg) then
-    send_document(get_receiver(msg), "./data/sticker/sudo.webp", ok_cb, false)
-      return "تو پارسا بابای منی"
+     --send_document(get_receiver(msg), "/data/sticker/sudo.webp ", ok_cb, false)
+      return "You are my father"
     elseif is_admin(msg) then
-    send_document(get_receiver(msg), "./data/sticker/admin.webp", ok_cb, false)
-      return "تو ادمین منی"
+   -- send_document(get_receiver(msg), "/data/sticker/admin.webp", ok_cb, false)
+      return "You are My ADMIN"
     elseif is_owner(msg) then
-    send_document(get_receiver(msg), "./data/sticker/owner.webp", ok_cb, false)
-      return "تو مدیر کل این گروهیی"
+   -- send_document(get_receiver(msg), "/data/sticker/owner.webp", ok_cb, false)
+      return "You are Group Owner"
     elseif is_momod(msg) then
-    send_document(get_receiver(msg), "./data/sticker/moderetion.webp", ok_cb, false)
-      return "تو مدیر گروهی"
+  --  send_document(get_receiver(msg), "/data/sticker/moderetion.webp", ok_cb, false)
+      return "You are Group Mod"
     else
-      send_document(get_receiver(msg), "./data/sticker/member.webp", ok_cb, false)
-      return "تو کاربر عادی هستی"
+  --  send_document(get_receiver(msg), "/data/sticker/member.webp", ok_cb, false)
+      return "You are Group Member"
+    end
   end
-end
 end
 
 return {
   patterns = {
-    "^[!/]([Mm]e)$",
-    "^([Mm]e)$",
-    "^([Ww]ai)$",
+    "^[#!/]([Mm]e)$",
+     "^([Mm]e)$"
     },
   run = run
 }
-end
+end 
