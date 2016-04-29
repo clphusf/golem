9b6228da930719a053a8a34a4b412da2c7a03447
parsa13881 <mohammadpa87@gmail.com>
2016-04-29 12:21:23 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -3,20 +3,20 @@ do
 local function run(msg, matches)
   if matches[1] == 'me' then
     if is_sudo(msg) then
-     send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/sudo.webp ", ok_cb, false)
       return "You are my father"
+           send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/sudo.webp ", ok_cb, false)
     elseif is_admin(msg) then
-   send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/admin.webp", ok_cb, false)
       return "You are My ADMIN"
+         send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/admin.webp", ok_cb, false)
     elseif is_owner(msg) then
-    send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/owner.webp", ok_cb, false)
       return "You are Group Owner"
+          send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/owner.webp", ok_cb, false)
     elseif is_momod(msg) then
-    send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/moderetion.webp", ok_cb, false)
       return "You are Group Mod"
+          send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/moderetion.webp", ok_cb, false)
     else
-    send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/member.webp", ok_cb, false)
       return "You are Group Member"
+          send_document(get_receiver(msg), "./home/fire/fire-sp/data/sticker/member.webp", ok_cb, false)
     end
   end
 end
