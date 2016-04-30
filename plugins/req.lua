130bde2db33293174cdeb79f3d8b49d0b6b25e84
golem1381 <pa.alemiii81@gmail.com>
2016-04-30 17:31:45 +0430

    Update req.lua

--- plugins/req.lua
+++ plugins/req.lua
@@ -1,16 +1,16 @@
 do
 
  function run(msg, matches)
- local ch = '1040275374'..msg.to.id
+ local ch = '122425165'..msg.to.id
  local fuse = 'New SuperGroup Request!\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "user#id"..105831687
+   local chat = "chat#id"..122425165
 
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'درخواست گروه شما فرستاده شد'
+  return 'Request Is Sent!\nYour SuperGroup Is Waiting For Create!'
 
  end
  end
@@ -23,3 +23,6 @@ do
   "^[#!/]reqgp$"
 
   },
+  run = run
+ }
+--by @MehdiHS
